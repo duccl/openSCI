f763f0cf11b47cae82ed4333f461a1b740c7cdd0
duccl <edr.ccol@gmail.com>
2020-09-11 22:40:19 -0300

    folders removed

--- __main__.py
+++
@@ -1,4 +0,0 @@
-from SCI import SCI
-
-if __name__ == "__main__":
-    SCI().deployAll()    
--- scripts/angular/main.sh
+++
@@ -1,7 +0,0 @@
-PROJECT_PATH=$1
-echo 'Deploying a Angular project'
-
-cd "$PROJECT_PATH"
-pwd
-
-echo npx react-scripts
